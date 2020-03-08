aae74d941cbe3091c10734f2781ec9087507164f
winton <mail@wintoni.us>
2020-03-07 19:31:26 -0800

    Generator

--- boiler.ts
+++ boiler.ts
@@ -1,40 +1,61 @@
+import { basename, join } from "path"
 import {
   InstallBoiler,
   PromptBoiler,
   GenerateBoiler,
-  UninstallBoiler,
 } from "boiler-dev"
 
-export const install: InstallBoiler = async ({
-  cwdPath,
-  files,
-}) => {
+export const install: InstallBoiler = async () => {
   const actions = []
+
+  actions.push({
+    action: "npmInstall",
+    dev: true,
+    source: ["serverless", "serverless-offline"],
+  })
+
   return actions
 }
 
 export const prompt: PromptBoiler = async ({
   cwdPath,
-  files,
+  allAnswers,
 }) => {
   const prompts = []
+
+  if (!allAnswers.pkgName) {
+    prompts.push({
+      type: "input",
+      name: "pkgName",
+      message: "project name (kebab-case)",
+      default: basename(cwdPath),
+    })
+  }
+
   return prompts
 }
 
 export const generate: GenerateBoiler = async ({
-  cwdPath,
-  answers,
+  allAnswers,
   files,
+  cwdPath,
 }) => {
   const actions = []
-  return actions
-}
 
-export const uninstall: UninstallBoiler = async ({
-  cwdPath,
-  answers,
-  files,
-}) => {
-  const actions = []
+  for (const file of files) {
+    const { name, source } = file
+
+    if (name === "serverless.yml") {
+      actions.push({
+        action: "write",
+        path: join(cwdPath, name),
+        source: source.replace(
+          /web-components-serverless-boiler/g,
+          allAnswers.pkgName
+        ),
+      })
+    }
+  }
+
   return actions
 }
